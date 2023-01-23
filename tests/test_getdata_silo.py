75866b959c667f16c09fcec50e589f95b9819bcb
Seb Haan <sebhaan@gmail.com>
2023-01-23 14:42:56 +1100

    Add tests for getdata_silo

---
+++ tests/test_getdata_silo.py
@@ -0,0 +1,41 @@
+# Tests for getdata_silo.py functions
+
+import os
+import pytest
+from geodata_harvester import getdata_silo
+
+def test_get_SILO_raster():
+    """
+    test script
+    """
+    layername = "daily_rain"
+    years = 2019
+    outpath = "silo_test"
+    bbox = (130, -44, 153.9, -11)
+    # test first for tif output format
+    format_out = "tif"
+    fnames_out = getdata_silo.get_SILO_raster(layername, years, outpath, bbox, format_out)
+    assert len(fnames_out) > 0
+    for fname in fnames_out:
+        assert os.path.exists(fname)
+        os.remove(fname)
+    print("get_SILO_raster test passed")
+
+
+def test_get_SILO_layers():
+    """
+    test script
+    """
+    layernames = ["daily_rain","max_temp"]
+    date_start = '2019-01-01'
+    date_end = '2020-02-01'
+    outpath = "silo_test"
+    bbox = (130, -44, 153.9, -11)
+    # test first for tif output format
+    format_out = "tif"
+    fnames_out = getdata_silo.get_SILO_layers(layernames, date_start, date_end, outpath, bbox, format_out)
+    assert len(fnames_out) > 0
+    for fname in fnames_out:
+        assert os.path.exists(fname)
+        os.remove(fname)
+    print("get_SILO_layers test passed")
