6961f7d8255a9dca409a334cf1dd428bacd95f48
Seb Haan <sebhaan@gmail.com>
2023-08-21 14:46:23 +1000

    Minor version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 from os import path
 import io
 
-PYPI_VERSION = '1.1.0'
+PYPI_VERSION = '1.1.1'
 
 this_directory = path.abspath(path.dirname(__file__))
 with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
